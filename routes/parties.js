30877ec08339a3a958170b1d5d432a5d8751e60c
vhraposo <thevhup@gmail.com>
2023-09-26 12:47:25 -0300

    feat: add update method at partycontroller

--- routes/parties.js
+++ routes/parties.js
@@ -10,4 +10,6 @@ router.route("/parties/:id").get((req, res) => partyController.get(req, res))
 
 router.route("/parties/:id").delete((req, res) => partyController.delete(req,res))
 
+router.route("/parties/:id").put((req, res) => partyController.update(req, res))
+
 module.exports = router
