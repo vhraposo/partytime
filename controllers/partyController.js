780ebfcd7ad20d6f4d78bd072e50fca28ba6dd18
vhraposo <thevhup@gmail.com>
2023-09-26 12:18:45 -0300

    feat: add post method at partyController

---
+++ controllers/partyController.js
@@ -0,0 +1,42 @@
+const PartyModel = require("../models/Party")
+
+const checkPartyBudget = (budget, services) => {
+
+    const priceSum = services.reduce((sum, service) => sum + service.price, 0)
+
+    if(priceSum > budget){
+        return false
+    }
+    return true
+}
+
+const PartyController = {
+    create: async(req, res) => {
+
+        try{
+
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services
+            }
+
+            if(party.services && !checkPartyBudget(party.budget, party.services)){
+                res.status(406).json({msg: "O ser orçamento é insuficiente."})
+                return
+            }
+            const response = await PartyModel.create(party)
+            res.status(201).json({response, msg: "Festa criada com sucesso!"})
+
+
+        } catch(error){
+            console.log(error)
+        }
+
+    }
+}
+
+module.exports = PartyController
--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,9 @@ const router = require("express").Router()
 
 //Services router
 const servicesRouter = require("./services")
-
 router.use("/", servicesRouter)
+
+const partyRouter = require("./parties")
+router.use("/", partyRouter)
 
 module.exports = router
