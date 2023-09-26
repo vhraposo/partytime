a9270605d8e784493437ddaefaae2b04703b556a
vhraposo <thevhup@gmail.com>
2023-09-26 12:36:46 -0300

    feat: add delete method at partycontroller

--- controllers/partyController.js
+++ controllers/partyController.js
@@ -54,6 +54,7 @@ const PartyController = {
 
             if(!party){
                 res.status(404).json({msg: "Festa não encontrada!"})
+                return
             }
 
             res.json(party)
@@ -61,7 +62,21 @@ const PartyController = {
         }catch(error){
             console.log(error)
         }
-    }
+    },
+
+    delete: async(req, res) => {
+
+        const id = req.params.id
+        const party = await PartyModel.findById(id)
+
+        if(!party){
+            res.status(404).json({msg: "Festa não encontrada!"})
+            return
+        }
+
+        const deletedParty = await PartyModel.findByIdAndDelete(id)
+        res.status(200).json({ deletedParty, msg: "Festa excluída com sucesso!"})
+    },
 }
 
 module.exports = PartyController
